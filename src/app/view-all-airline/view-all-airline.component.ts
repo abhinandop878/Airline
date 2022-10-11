c2808b790d5d0473f2724af0c531ecea5f3b0caa
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-11 16:44:38 +0530

    View All airline

---
+++ src/app/view-all-airline/view-all-airline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-airline',
+  templateUrl: './view-all-airline.component.html',
+  styleUrls: ['./view-all-airline.component.css']
+})
+export class ViewAllAirlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
